e8789a00aa9774792997f62fa9942285d312f08d
James Gilliland <neclimdul@gmail.com>
2025-08-15 03:07:17 -0500

    Remove hacks
    Hopefully not needed anymore

--- src/Hacks/Model/FormResponse.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace NecLimDul\MarketoRest\Hacks\Model;
-
-use NecLimDul\MarketoRest\Lead\Model\FormResponse as LeadFormResponse;
-use NecLimDul\MarketoRest\Lead\Model\Reason;
-
-class FormResponse extends LeadFormResponse {}
